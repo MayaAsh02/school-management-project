210a2bbf0c5b2fa4e615ef4c34aceb3338c781f3
Maya Ashkinazi <maya01911@gmail.com>
2023-08-04 01:28:50 +0300

    Change the DB to MySQL

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'schoolmanagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'school_managment',
+        'USER': 'django_user',
+        'PASSWORD': 'django_user',
+        'HOST': 'localhost',  # Replace with your MySQL host, e.g., 'localhost' or '127.0.0.1'
+        'PORT': '',           # Leave empty to use the default MySQL port (3306)
+
     }
 }
 
